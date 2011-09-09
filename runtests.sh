f5108b4ff1ea8f99b63fdfa71aa0c38730483ccb
Flavius Aspra <flavius.as@gmail.com>
2011-09-09 09:05:02 +0200

    use multiple php versions

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo -u flav TEST_PHP_EXECUTABLE=/usr/bin/php php /home/flav/php-5.3.6/run-tests.php \
+PHP_V=5.3.6
+
+PHP_D=$(echo $PHP_V | sed s/\\./_/g)
+
+echo $PHP_D
+
+sudo -u flav TEST_PHP_EXECUTABLE=/usr/local/php$PHP_V/bin/php /home/flav/projects/php/php_$PHP_D/run-tests.php \
 	-m \
 	--show-diff $*
